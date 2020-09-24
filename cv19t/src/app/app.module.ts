3a4d2a53d0937bdd70b349b8511a38feb4c4f492
Jackshooter <melarh98@gmail.com>
2020-09-24 16:01:36 +0800

    Succesfully create angular project AFTER

--- cv19t/src/app/app.module.ts
+++ cv19t/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderComponent} from './header/header.component';
 
 import { AppComponent } from './app.component';
+import { RecordNewTest } from './recordNewTest/recordnewtest.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from'@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecordNewTest
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cv19t/src/app/recordNewTest/recordnewtest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector:'app-new-test',
+  templateUrl:'./recordnewtest.component.html',
+  styleUrls:['./recordnewtest.component.css']
+})
+
+export class RecordNewTest {
+
+}
